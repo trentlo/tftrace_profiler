5f72b6795d55f9c4651ccfc99e129525f1e46353
Trent Lo <trentl@nvidia.com>
2018-08-10 17:43:00 -0700

    Add elaped time  - also we should caculate the percentage of op time using the elapsed time

--- src/analyzer.cpp
+++ src/analyzer.cpp
@@ -2,16 +2,23 @@
 #include <iostream>
 #include <map>
 #include <iomanip>
+#include <algorithm>
 
 namespace TFTraceProfiler {
 
 void Analyzer::analyze()
 {
+  size_t min_ts = UINT64_MAX;
+  size_t max_ts = 0;
+
   for (const JSONObj &o : objs_) {
     // only account for complete event
     if (o.ph != "X") {
       continue;
     }
+
+    max_ts = std::max(o.ts, max_ts);
+    min_ts = std::min(o.ts, min_ts);
 
     auto it = total_duration_.find(o.op_type);
     if (it!=total_duration_.end()) {
@@ -21,6 +28,8 @@ void Analyzer::analyze()
     }
   }
 
+  size_t elapsed_time = max_ts - min_ts;
+
   std::multimap<size_t, std::string> flip_map;
   size_t duration_sum = 0;
   for (auto i : total_duration_) {
@@ -29,6 +38,10 @@ void Analyzer::analyze()
   }
 
   // dump analysis results
+  std::cout << std::setw(10) << " total elapsed time = "
+            << elapsed_time/1000 << " ms"
+            << std::endl;;
+
   size_t id = 0;
   auto i = flip_map.rbegin();
   for (; i!=flip_map.rend(); ++i, ++id) {
@@ -39,8 +52,8 @@ void Analyzer::analyze()
     std::string op_type = i->second;
     size_t dur = i->first;
     std::cout << std::setw(30) << op_type << " : "
-              << std::setw(10) << dur << " ms"
-              << " (" << (double)dur/(double)duration_sum * 100.0 << "%)"
+              << std::setw(10) << dur << " us"
+              << " (" << (double)dur/(double)elapsed_time * 100.0 << "%)"
               << std::endl;;
   }
 }
